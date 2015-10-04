2a6fc2151f7c03a49e94b5fb9cf843f30e56c1b7
Abigail Watson <awatson1978@gmail.com>
2015-10-04 16:03:28 -0400

    Update multiActorColaboration.js

--- tests/nightwatch/walkthroughs/multiActorColaboration.js
+++ tests/nightwatch/walkthroughs/multiActorColaboration.js
@@ -1,10 +1,6 @@
 // add tests to this file using the Nightwatch.js API
 // http://nightwatchjs.org/api
 
-
-module.exports = {
-  "MultiActor Collaboration" : function (client) {
-
     var usernameA = "house";
     var usernameB = "camron";
 
@@ -21,24 +17,28 @@ module.exports = {
     var newPostDescription = "Words, words, words...";
     var newPostUrl = "http://www.wikipedia.com";
 
+
+module.exports = {
+  "A. Signing In UserA" : function (client) {
+
     client
       .url("http://localhost:3000")
       .resizeWindow(1024, 768)
 
-      .sectionBreak("A. Signing In UserA")
       .verify.elementPresent("#signInLink")
       .signIn(usernameA, usernameA)
       .verify.elementPresent("#collaborationListButton")
       .click("#collaborationListButton").pause(3000)
 
+  },
+  "B. UserA Can Review Collaborations" : function (client) {
 
-
-      .sectionBreak("B. UserA Can Review Collaborations")
+      .sectionBreak(")
       .reviewCollaborationListPage(collaborationName)
 
+  },
+  "C. UserA Can Add a Collaboration" : function (client) {
 
-
-      .sectionBreak("C. UserA Can Add a Collaboration")
         .click("#addCollaborationButton").pause(500)
         .reviewAddCollaborationPage(false, false, false, false, false, false, false)
         .addCollaboration(collaborationName, collaborationDescription, false, emailA, emailA, true, "")
@@ -46,8 +46,9 @@ module.exports = {
       .listOfCollaborationsContains(collaborationName, collaborationDescription)
 
 
+  },
+  "D. UserA Can Post To Collaboration" : function (client) {
 
-      .sectionBreak("D. UserA Can Post To Collaboration")
         .verify.elementPresent("#newPostLink")
         .click("#newPostLink").pause(1000)
         .waitForElementVisible("#postSubmitPage", 3000)
@@ -56,8 +57,9 @@ module.exports = {
         .pause(3000)
 
 
+  },
+  "E. View Collaboration Posts" : function (client) {
 
-      .sectionBreak("E. View Collaboration Posts")
         .click("#collaborationListButton").pause(500)
         .verify.elementPresent("#collaborationListPage")
         .verify.elementPresent("#collaborationsList")
@@ -66,9 +68,9 @@ module.exports = {
         .canSeePost(newPostTitle)
       .signOut(usernameA)
 
+  },
+  "F. UserB Requests To Join Collaboration" : function (client) {
 
-
-      .sectionBreak("F. UserB Requests To Join Collaboration")
         .signIn(usernameB, passwordB)
         .confirmUserIs(usernameB)
         .click("#collaborationListButton").pause(500)
@@ -78,8 +80,9 @@ module.exports = {
       .signOut(usernameB)
 
 
+  },
+  "G. UserA Grants Access to UserB" : function (client) {
 
-      .sectionBreak("G. UserA Grants Access to UserB")
       .signIn(usernameA, passwordA)
         .confirmUserIs(usernameA)
         .click("#collaborationListButton").pause(500)
@@ -87,11 +90,9 @@ module.exports = {
         .grantsCollaboration(emailB)
       .signOut(usernameA).pause(500)
 
+  },
+  "H. UserB Can Access Collaboration" : function (client) {
 
-      //============================================================================================
-
-
-      .sectionBreak("H. UserB Can Access Collaboration")
       .signIn(usernameB, passwordB)
         .confirmUserIs(usernameB)
         .click("#collaborationListButton").pause(500)
@@ -102,8 +103,9 @@ module.exports = {
       .signOut(usernameB)
 
 
+  },
+  "I. UserA Denies Access To UserB" : function (client) {
 
-      .sectionBreak("I. UserA Denies Access To UserB")
       .signIn(usernameA, passwordA)
         .confirmUserIs(usernameA)
         .click("#collaborationListButton").pause(500)
@@ -114,8 +116,9 @@ module.exports = {
       .signOut(usernameA)
 
 
+  },
+  "J. UserB Cant See Collaboration" : function (client) {
 
-      .sectionBreak("J. UserB Cant See Collaboration")
       .signIn(usernameB, passwordB)
         .confirmUserIs(usernameB)
         .click("#collaborationListButton").pause(1000)

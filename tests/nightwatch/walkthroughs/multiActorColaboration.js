6f430e32b3570844e43b5d3e566c491f51477ab7
Abigail Watson <awatson1978@gmail.com>
2015-10-04 19:15:50 -0400

    Update multiActorColaboration.js

--- tests/nightwatch/walkthroughs/multiActorColaboration.js
+++ tests/nightwatch/walkthroughs/multiActorColaboration.js
@@ -140,7 +140,7 @@ module.exports = {
 
 
 // -------------------------------------
-// OLD SCRIPT
+// BASIC SCRIPT
 
 // Signing In UserA
 // UserA Can Review Collaborations
@@ -155,18 +155,104 @@ module.exports = {
 
 
 // -------------------------------------
-// NEW SCRIPT
+// VERIFICATION SCRIPT
 
-// Signing In UserA
-// UserA Can Review Collaborations
-// UserA Can Add a Collaboration
-// UserA Can Post To Collaboration
-// View Collaboration Posts
-// UserB Requests To Join Collaboration
-// UserA Grants Access to UserB
+
+// When Not Signed In, UserA
+  // can List and View Public Collaborations 
+  // can List Private Collaborations
+  // can't View a Private Collaboration
+  // can't Add, Update or Delete any Collaborations
+
+  // can List and View the Foos of a Public Collaboration
+  // can't Add, Update, or Delete the Foos of a Public Collaboration
+  // can't List, View, Add, Delete the Foos of a Private Collaboration
+
+
+// When Signed In, UserA
+  // can List and View Public Collaborations 
+  // can List Private Collaborations
+  // can View, Upsert, and Delete Private Collaboration they Own
+  // can't View, Upsert, and Delete Private Collaboration they don't Own
+
+  // can List and View the Foos of a Public Collaboration
+  // can't Add, Update, or Delete the Foos of a Public Collaboration they're not Members Of
+  // can List, View, Add, Update, and Delete the Foos of a Collaboration they're Members Of
+  // can't List, View, Add, Update, or Delete the Foos of a Private Collaboration they're not Members Of
+
+
+// -------------------------------------
+// VALIDATION COMMANDS
+
+
+  // {{User}} Has Default Public Access to Collaborations 
+    // User Can List All Collaborations 
+    // User Can List Private Collaborations 
+    // User Can List Public Collaborations 
+    // User Can View Details of Public Collaborations 
+
+  // {{User}} Has SignedOut Access to Collaborations 
+    // User Can't Add, Update, or Delete Any Collaborations
+    // User Can't View Details of Any Private Collaborations
+    // User Can List and View Foos of Public Collaborations
+    // User Can't Upsert or Delete Foos of Public Collaborations
+    // User Can't List, View Details, Upsert, Or Delete Foos of Any Private Collaborations
+
+
+  // {{User}} Has SignedIn Access to Collaborations 
+    // User Can't Add, Update, or Delete Collaborations
+    // User Can't View Details of Private Collaborations they're not Members Of 
+    // User Can View Private Collaborations they're Members Of 
+
+    // User Can List and View Foos of Public Collaborations
+    // User Can List, View, Upsert and Delete Foos of Collaborations they're Members Of
+    // User Can't List, View, Upsert or Delete Foos of Collaborations they're not Members Of
+
+// -------------------------------------
+// VALIDATION SCRIPT
+
+
+// Begin
+
+  // UserA Has Default Public Access to Collaborations 
+  // UserA Has SignedOut Access to Collaborations 
+
+
+// Sign In UserA
+
+  // UserA Has Default Public Access to Collaborations 
+  // UserA Has SignedOut Access to Collaborations 
+
+  // UserA creates Public Collaboration "AwesomeArdvaarks"
+  // UserA creates Public Collaboration "BusyBees"
+
+  // UserB Has Default Public Access to Collaborations 
+  // UserB Has SignedOut Access to Collaborations 
+
+  // UserB can View Details of AwesomeArdvarks
+  // UserB can View Details of BusyBees
+
+    // User Can't Add, Update, or Delete Any Collaborations
+    // User Can't View Details of Any Private Collaborations
+    // User Can List and View Foos of Public Collaborations
+    // User Can't Upsert or Delete Foos of Public Collaborations
+    // User Can't List, View Details, Upsert, Or Delete Foos of Any Private Collaborations
+
+
+
+  // UserB Requests To Join Collaboration
+
+    // UserA Grants Access to UserB
+    // UserB signs in
+
+    
+
+
 // UserB Can Access Collaboration
 // UserA Denies Access To UserB
 // UserB Cant See Collaboration
+
+
 
 
 

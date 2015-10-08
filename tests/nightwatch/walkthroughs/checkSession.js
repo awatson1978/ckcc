1ac23a0eaae52b58736cadc2e07603bb42cdcb57
Abigail Watson <awatson1978@gmail.com>
2015-10-07 20:23:53 -0700

    checkSession is experimental; lets try helloGoogle?hell

--- tests/nightwatch/walkthroughs/checkSession.js
+++ tests/nightwatch/walkthroughs/checkSession.js
@@ -1,17 +1,17 @@
-
-var sessionBar = false;
-var glassOpacity = 0;
-var fooVal = false;
-
-//var expect = require('chai').expect;
-
-module.exports = {
-  "Check Session" : function (client) {
-    client
-      .url("http://localhost:3000")
-      .resizeWindow(1024, 768)
-      .pause(1000)
-      .checkSession('appWidth', 1024, 5000)
-      .end();
-  }
-};
+//
+// var sessionBar = false;
+// var glassOpacity = 0;
+// var fooVal = false;
+//
+// //var expect = require('chai').expect;
+//
+// module.exports = {
+//   "Check Session" : function (client) {
+//     client
+//       .url("http://localhost:3000")
+//       .resizeWindow(1024, 768)
+//       .pause(1000)
+//       .checkSession('appWidth', 1024, 5000)
+//       .end();
+//   }
+// };

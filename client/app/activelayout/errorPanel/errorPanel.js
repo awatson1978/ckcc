50d8b9d57aebdaf6f17cf864eb611f6594b86346
Abigail Watson <awatson1978@gmail.com>
2015-10-25 23:07:28 -0700

    add/join collaborations works like a charm

--- client/app/activelayout/errorPanel/errorPanel.js
+++ client/app/activelayout/errorPanel/errorPanel.js
@@ -1,15 +1,39 @@
 Session.setDefault("errorMessage", false);
+Session.setDefault("successMessage", false);
 
 Template.errorPanel.helpers({
-  alertPanelVisibility: function (){
-    if (Session.get('errorMessage')){
+  successPanelVisibility: function (){
+    if (Session.get('successMessage')){
+      Meteor.setTimeout(function (){
+        Session.set("successMessage", false);
+      }, 3000);
       return "opacity: 1; z-index: 1000; top: 0px;";
     } else {
       return "opacity: 0; z-index: -1; top: -50px;";
     }
   },
+  errorPanelVisibility: function (){
+    if (Session.get('errorMessage')){
+      Meteor.setTimeout(function (){
+        Session.set("errorMessage", false);
+      }, 3000);
+      return "opacity: 1; z-index: 1000; top: 0px;";
+    } else {
+      return "opacity: 0; z-index: -1; top: -50px;";
+    }
+  },
+  getSuccessMessage: function (){
+    return Session.get('successMessage');
+  },
   getErrorMessage: function (){
     return Session.get('errorMessage');
+  },
+  hasSuccessMessage: function (){
+    if (Session.get('successMessage')) {
+      return true;
+    } else {
+      return false;
+    }
   },
   hasErrorMessage: function (){
     if (Session.get('errorMessage')) {
@@ -22,6 +46,9 @@ Template.errorPanel.helpers({
 
 
 Template.errorPanel.events({
+  "click #successPanel": function (event, template){
+    Session.set('successMessage', false);
+  },
   "click #errorPanel": function (event, template){
     Session.set('errorMessage', false);
   }

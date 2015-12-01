e56435ab5ce1704ff8fcee2968c70db705ac40ef
Abigail Watson <awatson1978@gmail.com>
2015-11-30 16:57:19 -0800

    remove Questionnaires init script

--- server/startup.js
+++ server/startup.js
@@ -31,9 +31,9 @@ Meteor.startup(function () {
     if (Studies.find().count() === 0) {
       Meteor.call("initializeStudies");
     }
-    if (Questionnaires.find().count() === 0) {
-      Meteor.call("initializeQuestionnaires");
-    }
+    // if (Questionnaires.find().count() === 0) {
+    //   Meteor.call("initializeQuestionnaires");
+    // }
     if (Meteor.users.find().count() === 0) {
       Meteor.call("iniitalizeUsers");
     }

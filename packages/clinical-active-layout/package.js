92ea7eee81b4c0586fd10079387c532a10c2e615
Abigail Watson <awatson1978@gmail.com>
2015-12-19 17:26:23 -0800

    added clinical.meteor.rc8.json

--- packages/clinical-active-layout/package.js
+++ packages/clinical-active-layout/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:active-layout',
-  version: '0.4.1',
+  version: '0.4.2',
   summary: 'Layout templates with keybindings and animations.',
   git: 'http://github.com/clinical-meteor/clinical-active-layout',
   documentation: 'README.md'

51c4941a9a0d256adca27d780f757475bf60edcb
Abigail Watson <awatson1978@gmail.com>
2015-12-21 15:24:07 -0800

    super simple menu page

--- packages/medbook-samples/lib/Samples.js
+++ packages/medbook-samples/lib/Samples.js
@@ -12,127 +12,127 @@ Samples.allow({
   }
 });
 
-SampleSchema = new SimpleSchema({
-  "_id": {
-    type: String,
-    optional: true
-  },
-  "name": {
-    type: String,
-    optional: true,
-    defaultValue: "",
-    label: "Name"
-  },
-  "short_name": {
-    type: String,
-    optional: true,
-    defaultValue: "",
-    label: "Short Name"
-  },
-  "description": {
-    type: String,
-    optional: true,
-    defaultValue: "",
-    label: "Description"
-  },
-  "id": {
-    type: String,
-    optional: true,
-    defaultValue: "",
-    label: "Sample Slug"
-  },
-  "cbio_id": {
-    type: String,
-    optional: true,
-    defaultValue: "",
-    label: "CBIO"
-  },
-  "type_of_cancer_id": {
-    type: String,
-    optional: true,
-    defaultValue: "",
-    label: "Type of Cancer"
-  },
-  "public": {
-    type: Boolean,
-    optional: true,
-    defaultValue: true,
-    label: "Public"
-  },
-  "pmid": {
-    type: String,
-    optional: true,
-    defaultValue: "",
-    label: "PMID"
-  },
-  "slug": {
-    type: String,
-    optional: true,
-    defaultValue: "",
-    label: "Slug"
-  },
-  "citation": {
-    type: String,
-    optional: true,
-    defaultValue: "unpublished",
-    label: "Citation"
-  },
-  "tables": {
-    type: [String],
-    optional: true,
-    defaultValue: [],
-    label: "Tables",
-    autoform: {
-      afArrayField: {
-        template: "photonic"
-      }
-    }
-  },
-  "collaborations": {
-    type: [String],
-    optional: true,
-    defaultValue: [],
-    label: "Collaborations",
-    autoform: {
-      afArrayField: {
-        template: "photonic"
-      }
-    }
-  },
-  "Sample_IDs": {
-    type: [String],
-    optional: true,
-    defaultValue: [],
-    label: "Sample IDs",
-    autoform: {
-      afArrayField: {
-        template: "photonic"
-      }
-    }
-  },
-  "Patient_IDs": {
-    type: [String],
-    optional: true,
-    defaultValue: [],
-    label: "Patient IDs",
-    autoform: {
-      afArrayField: {
-        template: "photonic"
-      }
-    }
-  },
-  "Questionnaires": {
-    type: [String],
-    optional: true,
-    defaultValue: [],
-    label: "Questionnaires",
-    autoform: {
-      afArrayField: {
-        class: "questionnaires",
-        template: "photonic"
-      }
-    }
-  }
-
-});
-Samples.attachSchema(SampleSchema);
+// SampleSchema = new SimpleSchema({
+//   "_id": {
+//     type: String,
+//     optional: true
+//   },
+//   "name": {
+//     type: String,
+//     optional: true,
+//     defaultValue: "",
+//     label: "Name"
+//   },
+//   "short_name": {
+//     type: String,
+//     optional: true,
+//     defaultValue: "",
+//     label: "Short Name"
+//   },
+//   "description": {
+//     type: String,
+//     optional: true,
+//     defaultValue: "",
+//     label: "Description"
+//   },
+//   "id": {
+//     type: String,
+//     optional: true,
+//     defaultValue: "",
+//     label: "Sample Slug"
+//   },
+//   "cbio_id": {
+//     type: String,
+//     optional: true,
+//     defaultValue: "",
+//     label: "CBIO"
+//   },
+//   "type_of_cancer_id": {
+//     type: String,
+//     optional: true,
+//     defaultValue: "",
+//     label: "Type of Cancer"
+//   },
+//   "public": {
+//     type: Boolean,
+//     optional: true,
+//     defaultValue: true,
+//     label: "Public"
+//   },
+//   "pmid": {
+//     type: String,
+//     optional: true,
+//     defaultValue: "",
+//     label: "PMID"
+//   },
+//   "slug": {
+//     type: String,
+//     optional: true,
+//     defaultValue: "",
+//     label: "Slug"
+//   },
+//   "citation": {
+//     type: String,
+//     optional: true,
+//     defaultValue: "unpublished",
+//     label: "Citation"
+//   },
+//   "tables": {
+//     type: [String],
+//     optional: true,
+//     defaultValue: [],
+//     label: "Tables",
+//     autoform: {
+//       afArrayField: {
+//         template: "photonic"
+//       }
+//     }
+//   },
+//   "collaborations": {
+//     type: [String],
+//     optional: true,
+//     defaultValue: [],
+//     label: "Collaborations",
+//     autoform: {
+//       afArrayField: {
+//         template: "photonic"
+//       }
+//     }
+//   },
+//   "Sample_IDs": {
+//     type: [String],
+//     optional: true,
+//     defaultValue: [],
+//     label: "Sample IDs",
+//     autoform: {
+//       afArrayField: {
+//         template: "photonic"
+//       }
+//     }
+//   },
+//   "Patient_IDs": {
+//     type: [String],
+//     optional: true,
+//     defaultValue: [],
+//     label: "Patient IDs",
+//     autoform: {
+//       afArrayField: {
+//         template: "photonic"
+//       }
+//     }
+//   },
+//   "Questionnaires": {
+//     type: [String],
+//     optional: true,
+//     defaultValue: [],
+//     label: "Questionnaires",
+//     autoform: {
+//       afArrayField: {
+//         class: "questionnaires",
+//         template: "photonic"
+//       }
+//     }
+//   }
+//
+// });
+// Samples.attachSchema(SampleSchema);
